5ab9cf5b9b1be920d1fa6b33bf3ed68ed6fc87aa
Lucas Andrade <lafdesouza2002@outlook.com>
2024-01-26 01:53:29 -0300

    test: Create helpers test

---
+++ helpers/helpers_test.go
@@ -0,0 +1,41 @@
+package helpers_test
+
+import (
+	"testing"
+
+	"github.com/olucasandrade/url-analyzer/helpers"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestConvertPageSizeToMB(t *testing.T) {
+	page_size := 1
+	page_size_in_MB := helpers.ConvertPageSizeToMB(page_size)
+
+	assert.Equal(t, "1MB", page_size_in_MB)
+}
+
+func TestHasHTTPPrefix(t *testing.T) {
+	url_with_http := "http://www.criaway.com"
+	url_without_http := "www.criaway.com"
+	empty_string := ""
+
+	assert.Equal(t, true, helpers.HasHTTPPrefix(url_with_http))
+	assert.Equal(t, false, helpers.HasHTTPPrefix(url_without_http),)
+	assert.Equal(t, false, helpers.HasHTTPPrefix(empty_string))
+}
+
+func TestIsValidURL(t *testing.T) {
+	valid_url := "http://www.criaway.com"
+	invalid_url_with_http := "http://something..."
+	url_without_http := "criaway.com"
+	email := "lafdesouza2002@gmail.com"
+	empty_string := ""
+	special_characters := "!@#$ˆ&*()"
+
+	assert.Equal(t, true, helpers.IsValidURL(valid_url))
+	assert.Equal(t, false, helpers.IsValidURL(invalid_url_with_http))
+	assert.Equal(t, false, helpers.IsValidURL(url_without_http))
+	assert.Equal(t, false, helpers.IsValidURL(email))
+	assert.Equal(t, false, helpers.IsValidURL(empty_string))
+	assert.Equal(t, false, helpers.IsValidURL(special_characters))
+}
