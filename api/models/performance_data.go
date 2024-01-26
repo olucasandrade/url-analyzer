1a8a4574bdc08f56b083e2035c4cfbdd78a23aee
Lucas Andrade <lafdesouza2002@outlook.com>
2024-01-26 00:15:41 -0300

    fix: Remove api scheme

--- api/models/performance_data.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type PerformanceData struct {
-    LoadTime           string `json:"loadTime"`
-    HTTPRequestsCount  int           `json:"httpRequestsCount"`
-    PageSize           string           `json:"pageSize"`
-}
